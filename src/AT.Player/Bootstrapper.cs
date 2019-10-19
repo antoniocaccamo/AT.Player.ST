2090eb8d1a54d915cd7e4f21cd6bd4aa628c5d07
antonio caccamo <caccamo.antonio@gmail.com>
2019-10-20 00:24:44 +0200

    UPD : ffmpeg

--- src/AT.Player/Bootstrapper.cs
+++ src/AT.Player/Bootstrapper.cs
@@ -7,14 +7,21 @@ namespace AT.Player
 {
     public class Bootstrapper : Bootstrapper<ShellViewModel>
     {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
         protected override void Configure()
         {
             // Perform any other configuration before the application starts
 
             Unosquare.FFME.Library.FFmpegDirectory =
-                @"C:\development\tools\ffmpeg\ffmpeg-4.2.1-win32-static\";
+                @"C:\development\tools\ffmpeg\ffmpeg-4.2.1-win32-shared\bin";
             // Unosquare.FFME.Library.EnableWpfMultiThreadedVideo = true;
-            System.Console.WriteLine("Configure..");
+            Logger.Info("Configure Unosquare.FFME.Library.FFmpegDirectory) [{0}]" +
+                " - Exists [{1}]",
+                Unosquare.FFME.Library.FFmpegDirectory,
+                System.IO.Directory.Exists(Unosquare.FFME.Library.FFmpegDirectory));
+
+            Unosquare.FFME.Library.LoadFFmpeg();
         }
 
         protected override void OnStart()
